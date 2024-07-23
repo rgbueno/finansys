22b9426a4cbbf1fc93e76659edebfd4e74037894
Rodrigo Gomes Bueno <rodrigobueno.ti@gmail.com>
2024-07-23 15:31:08 -0300

    Adicionando modulo de categorias

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CategoryListComponent },
+  { path: ':id/edit', component: CategoryFormComponent },
+  { path: 'new', component: CategoryFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
