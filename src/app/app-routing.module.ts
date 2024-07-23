5f0312180cf879cd52e81926c6c8e8232e4cecfc
Rodrigo Gomes Bueno <rodrigobueno.ti@gmail.com>
2024-07-23 18:20:53 -0300

    Adicionando navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule'    
-  }
+  //{ path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' }
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) } 
 ];
 
 @NgModule({
