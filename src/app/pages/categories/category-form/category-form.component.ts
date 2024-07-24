a41b22e290fab8bd8e710f496a194ae7b8cb7a4f
Rodrigo Gomes Bueno <rodrigobueno.ti@gmail.com>
2024-07-24 19:09:40 -0300

    Implementando componente category-form

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -6,8 +6,8 @@ import { Category } from "../shared/category.model";
 import { CategoryService } from "../shared/category.service";
 
 import { switchMap } from 'rxjs/operators';
-import { toastr } from "toastr";
-import { ActiveDataPoint } from 'chart.js';
+import * as toastr from "toastr"
+
 
 @Component({
   selector: 'app-category-form',
@@ -20,7 +20,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   categoryForm: FormGroup;
   pageTitle: string;
   serverErrorMessages: string[] = null;
-  submitingForm: boolean = false;
+  submittingForm: boolean = false;
   category: Category = new Category();
 
   constructor(
@@ -76,6 +76,56 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
       const categoryName = this.category.name || "";
       this.pageTitle = "Editanto categoria:" + categoryName;
     }
+  }
+
+  submitForm(){
+    this.submittingForm = true;
+
+    if(this.currentAction == "new")
+      this.createCategory();
+    else
+      this.uptadeCategory();
+  }
+
+  createCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      );
+
+  }
+
+  uptadeCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.update(category)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    );
+  }
+
+  private actionsForSuccess(category: Category){
+    toastr.success("Solicitação processada com sucesso.");
+
+    //força recarregamento do componente
+    this.router.navigateByUrl("categories", {skipLocationChange: true}).then( 
+      () => this.router.navigate(["categories", category.id, "edit"])
+    );
+  }
+
+  private actionsForError(error){
+    toastr.error("Ocorreu um erro ao processar sua solicitação.");
+
+    this.submittingForm = false;
+
+    if(error.status === 422)
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    else
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente novamente mais tarde"];
   }
 
 }
